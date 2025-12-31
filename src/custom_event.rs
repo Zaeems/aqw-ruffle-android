51895d31bcc63491aa6767022a484f179ff94a29
Z P <46079004+Zaeems@users.noreply.github.com>
2025-12-31 06:21:57 +0000

    Initial commit

---
+++ src/custom_event.rs
@@ -0,0 +1,18 @@
+//! Custom event type for Ruffle on Android
+
+use ruffle_core::events::KeyDescriptor;
+
+/// User-defined events.
+#[derive(Debug)]
+pub enum RuffleEvent {
+    /// Indicates that one or more tasks are ready to poll on our executor.
+    TaskPoll,
+    VirtualKeyEvent {
+        down: bool,
+        key_descriptor: KeyDescriptor,
+    },
+    TextInput { codepoint: char },
+    RunContextMenuCallback(usize),
+    ClearContextMenu,
+    RequestContextMenu,
+}
